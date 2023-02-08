74d22e9eb608eba43b79c513544386aaba80e5d7
laurenkumar <lauren_d@outlook.fr>
2023-02-08 13:40:00 +0100

    first commit

--- htmlcours/src/pages/index.tsx
+++ htmlcours/src/pages/index.tsx
@@ -3,7 +3,12 @@ import { KodemoPlayer } from '@kodemo/player';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
-const Index = () => {
+interface TextOptions {
+    language?: string;
+    story?: string;
+}
+
+const Index = (opts: TextOptions) => {
 
   return (
     <Main
