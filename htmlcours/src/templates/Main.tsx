68abd36add36bb39088782c59b1aa894aec164b0
laurenkumar <lauren_d@outlook.fr>
2023-02-08 14:39:22 +0100

    first commit

--- htmlcours/src/templates/Main.tsx
+++ htmlcours/src/templates/Main.tsx
@@ -9,16 +9,15 @@ type IMainProps = {
 };
 
 const Main = (props: IMainProps) => (
-  <div className="w-full px-1 text-gray-700 antialiased">
+  <div>
     {props.meta}
 
 
-      <div className="content py-5 text-xl">{props.children}</div>
+      <div>{props.children}</div>
 
-      <div className="border-t border-gray-300 py-8 text-center text-sm">
+      <div>
         © Copyright {new Date().getFullYear()} {AppConfig.title}.
       </div>
-    </div>
   </div>
 );
 
