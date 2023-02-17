e0a3e895d32bb3bd595a210fb99979f813ff4618
laurenkumar <lauren_d@outlook.fr>
2023-02-17 11:43:21 +0100

    top menu reste sidebar

--- htmlcours/src/pages/index.tsx
+++ htmlcours/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { KodemoPlayer, Pagination } from '@kodemo/player';
 import { KodemoMenu } from '@kodemo/util';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 /* tslint:disable */
 import { Meta } from '@/layouts/Meta';
@@ -9,6 +9,12 @@ import { Main } from '@/templates/Main';
 import html_part_1 from 'src/cours/html_part_1.json';
 
 const Index = () => {
+  const [sidebar, setSidebar] = useState("");
+
+  useEffect(() => {
+    setSidebar(html_part_1.documentNav);
+    console.log(sidebar.sousparties); 
+  }, [sidebar]);
 
   function Menu() {
     return (
@@ -28,9 +34,61 @@ const Index = () => {
     );
   }
 
-  useEffect(() => {
-    console.log(html_part_1.documentNav)
-  }, []);
+  function SideNav() {
+    return (
+      <nav className="side">
+        <div className="side__program">
+          <div className="side__program__course">
+            <div className="side__program__course__icon">
+              <span className="dashicons dashicons-admin-appearance">📜</span>
+            </div>
+            <div className="side__program__course__title">
+              <small>Formation</small><br/>
+              Responsive Web Design: créer un site web sur mesure
+            </div>
+          </div>
+                <div>
+                  <div className="side__program__part">
+                    <div className="side__program__part__picto">
+                      ❤️
+                    </div>
+                    <div className="side__program__part__title">
+                      <span>Projet •</span> {sidebar.sujet}
+                    </div>
+                  </div>
+                  <div className="side__program__chapter">
+                    <div className="side__program__chapter__num">1A</div>
+                    <div className="side__program__chapter__text">{sidebar.partie}</div>
+                  </div> 
+                  <ul className="side__program__lessons">
+                    {sidebar.sousparties.map((data, idx) => (
+                    <li className="side__program__lesson">
+                      <a href={data.id}>{idx+1}. {data.titre}</a>
+                    </li>
+                    ))}
+                  </ul>
+                </div>
+          <div className="side__program__part">
+            <div className="side__program__part__picto">
+              ❤️
+            </div>
+            <div className="side__program__part__title">
+              <span>Conclusion •</span> Vous savez maintenant créer un site statique !
+            </div>
+          </div>
+
+          <ul className="side__program__lessons">
+            <li
+              className="side__program__lesson"
+            >
+              <a>1. Conclusion : et maintenant ?</a>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+    );
+  }
 
   return (
     <Main
@@ -41,6 +99,7 @@ const Index = () => {
         />
       }
     >
+      {sidebar && (<SideNav />)}
       <KodemoPlayer
         menu={<Menu />}
         keyboardPagination={true} 
