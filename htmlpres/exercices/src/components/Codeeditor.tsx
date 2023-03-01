b9948cb6107eb5fcc6af4b3a6640c1c2a0dd76e6
laurenkumar <lauren_d@outlook.fr>
2023-03-01 17:47:09 +0100

    test

--- htmlpres/exercices/src/components/Codeeditor.tsx
+++ htmlpres/exercices/src/components/Codeeditor.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useLocalStorage } from "../Hooks/LocalStorage";
 import Editor, { Monaco } from "@monaco-editor/react";
-import runTests from '../pages/test.jsx';
 import Modal from '../components/Modal.tsx';
 
 function CodeOnline({subject}) {
