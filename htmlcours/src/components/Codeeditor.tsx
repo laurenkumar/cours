189d12c311ba6c7cbd803210e06dc31f727eff61
laurenkumar <33550937+laurenkumar@users.noreply.github.com>
2024-02-02 17:39:23 +0100

    refactorisation test

--- htmlcours/src/components/Codeeditor.tsx
+++ htmlcours/src/components/Codeeditor.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useLocalStorage } from "../Hooks/LocalStorage";
-import { useDebouncedState } from "../Hooks/useDebounceState";
+import { useDebouncedState } from "../Hooks/useDebouncedState";
 import Editor, { Monaco } from "@monaco-editor/react";
 import Modal from '../components/Modal.tsx';
 
