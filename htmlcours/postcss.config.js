19edf44938a49701b13a3363a51ecbf5968c8e88
laurenkumar <33550937+laurenkumar@users.noreply.github.com>
2024-02-02 20:20:17 +0100

    refactorisation test

--- htmlcours/postcss.config.js
+++ htmlcours/postcss.config.js
@@ -2,9 +2,21 @@
 // it will create an unexpected error: Invalid PostCSS Plugin found: [0]
 
 module.exports = {
-  plugins: {
+  plugins: 
+  {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+    ...(process.env.NODE_ENV === 'production' ? {
+      cssnano: {},
+      '@fullhuman/postcss-purgecss': {
+        content: [
+          './pages/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}'
+        ],
+        // Include any other file extensions your project uses
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ["html", "body"]
+      },
+    } : {}),
   },
 };
