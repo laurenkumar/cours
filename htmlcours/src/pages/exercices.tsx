398e7cb906296ee3774d55e53d03098e4024217b
laurenkumar <33550937+laurenkumar@users.noreply.github.com>
2024-02-03 19:17:30 +0100

    bug login

--- htmlcours/src/pages/exercices.tsx
+++ htmlcours/src/pages/exercices.tsx
@@ -1,11 +1,10 @@
 import CodeOnline from '../components/Codeeditor';
-import React from 'react';
 
 /* tslint:disable */
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 import { useRouter } from "next/router";
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Exercices = React.memo(() => {
   const router = useRouter();
