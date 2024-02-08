aa7f67459a06c3dfc34840d0f8a7e30105b82ab2
laurenkumar <33550937+laurenkumar@users.noreply.github.com>
2024-02-08 20:23:02 +0100

    exc19

--- htmlpres/exercices/src/Hooks/LocalStorage.tsx
+++ htmlpres/exercices/src/Hooks/LocalStorage.tsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 
 export const useLocalStorage = (key, initialValue) => {
-  const [storedValue, setStoredValue] = useState(initialValue);
-
-  useEffect(() => {
-    const item = localStorage.getItem(key);
-    if (item) {
-      setStoredValue(JSON.parse(item));
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch {
+      return initialValue;
     }
-  }, [key]);
+  });
 
   const setValue = (value) => {
     try {
